6c4d0f1a3fd37637961f227efb797c42061e7228
Disha Sethi <dishasethi01.1@gmail.com>
2026-01-28 23:40:58 +0530

    Update button text to 'Return to Hangar area'

--- src/components/MobileChatLayout.js
+++ src/components/MobileChatLayout.js
@@ -90,7 +90,7 @@ export default function MobileChatLayout({ userId }) {
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={3} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
                 </svg>
-                <span>Return to Hangar</span>
+                <span>Return to Hangar area </span>
               </button>
             </div>
             
